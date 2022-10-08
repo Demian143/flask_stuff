0b0a00042392d09a778f679a2c839e0e1c18563f
Demian143 <patrickxavier140@gmail.com>
2022-10-08 18:20:13 -0300

    adding postgres container and sqlalchemy config

--- app.py
+++ app.py
@@ -11,10 +11,15 @@ def create_app(app) -> Flask:
     add_rsc(api)
     api.init_app(app)
 
-    # db = SQLAlchemy()
-    # db.init_app(app)
+    db = SQLAlchemy()
+    app_config(app)
+    db.init_app(app)
 
     return app
+
+
+def app_config(app):
+    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://aori143:postgres@0.0.0.0:5432/postgres"
 
 
 def add_rsc(api):
