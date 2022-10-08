c4fcb18676cd029804e0dbd95469073530a2c6d3
Demian143 <patrickxavier140@gmail.com>
2022-10-08 16:48:32 -0300

    aplying factor structure and adding sqlalchemy to the game

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api
 from crud import hello_world
 
 app = Flask(__name__)
-api = Api(app)
 
 
-api.add_resource(hello_world.HelloWorld, '/')
+def create_app(app) -> Flask:
+    api = Api()
+    add_rsc(api)
+    api.init_app(app)
+
+    # db = SQLAlchemy()
+    # db.init_app(app)
+
+    return app
+
+
+def add_rsc(api):
+    api.add_resource(hello_world.HelloWorld, '/')
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    create_app(app).run()
