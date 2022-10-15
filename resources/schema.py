6968150ff59d53d0b48659a673afbf10bfd75c10
Demian143 <patrickxavier140@gmail.com>
2022-10-15 12:15:06 -0300

    schemas for each of the models

--- resources/schema.py
+++ resources/schema.py
@@ -1,9 +1,34 @@
 from flask import jsonify
 
 
-def schema(record: object) -> jsonify:
-    band = record.band
-    album = record.album
-    genre = record.genre if record.genre else None
+def schema(obj: object) -> jsonify:
+    """ Supports Song and Band object and returns a json. """
+    id = obj.id
+    name = obj.name
 
-    return jsonify(band=band, album=album, genre=genre)
+    return jsonify(id=id, name=name)
+
+
+def genre_schema(obj: object) -> jsonify:
+    """ Supports genre object and returns a json. """
+    id = obj.id
+    genre = obj.genre
+
+    return jsonify(id=id, genre=genre)
+
+
+def album_schema(obj: object) -> jsonify:
+    """ Returns a json representation of the album schema. """
+
+    id = obj.id
+    name = obj.name
+
+    song_id = obj.song_id if obj.song_id else None
+    band_id = obj.band_id if obj.band_id else None
+    genre_id = obj.genre_id if obj.genre_id else None
+
+    return jsonify(id=id,
+                   name=name,
+                   song_id=song_id,
+                   band_id=band_id,
+                   genre_id=genre_id)
